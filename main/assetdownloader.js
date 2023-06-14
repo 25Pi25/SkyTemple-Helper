48d80ef7ab4a1102c8b3fe4ef1dae43d39ee7200
25Pi25 <25pi25@mail.com>
2023-06-14 01:01:15 -0700

    Updated to Generation 9 & fixed index-only dependencies

--- main/assetdownloader.js
+++ main/assetdownloader.js
@@ -1,5 +1,4 @@
 async function updateTertiaryOption() {
-    console.log(selectedVariant)
     document.getElementById('portraitPreview').innerHTML = `<img src="${selectedVariant.portraits.previewEmotion ? selectedVariant.portraits.sheetUrl : ''}" alt="No Portraits." style="height: 410px; width: auto;">`
     document.getElementById('spritePreview').innerHTML = `<img src="${selectedVariant.sprites.recolorSheetUrl}" alt="No Sprites." style="height: 312px; width: auto;">`
     document.getElementById("downloadPortraits").disabled = disabledButtons.portrait || !selectedVariant.portraits.previewEmotion
--- src/functions/getPokemonData.js
+++ src/functions/getPokemonData.js
@@ -2,15 +2,14 @@ const axios = require("axios");
 const pokemonArray = require('../../assets/pokemon.json');
 
 exports.handler = async (event, pokemon) => {
-    pokemon = pokemonArray.indexOf(pokemon)
     const { data } = await axios.request("https://spriteserver.pmdcollab.org/graphql", {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         data: {
-            query: `query ($pokemon: Int!) { monster(filter:[$pokemon]) { id name forms { fullName portraits{ sheetUrl previewEmotion { url }} sprites{ zipUrl animDataXml recolorSheetUrl }}}}`,
+            query: `query ($pokemon: String!) { searchMonster(monsterName: $pokemon) { id name forms { fullName portraits{ sheetUrl previewEmotion { url }} sprites{ zipUrl animDataXml recolorSheetUrl }}}}`,
             variables: { pokemon }
         }
     })
 
-    return data.data.monster[0]
+    return data.data.searchMonster[0]
 }
